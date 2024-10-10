595293ec28f5ce20380767127e062a8b32783065
dion streefkerk <dionstreefkerk10@gmail.com>
2024-10-10 20:02:41 +0200

    Added Bootstrap and highlight.js imports to main.js
    - Imported Bootstrap CSS and JavaScript to enhance the styling and layout of the Vue components.
    - Added highlight.js for code syntax highlighting in note content across the application.
    - Integrated both libraries globally to be available throughout the Vue app.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'highlight.js/styles/atom-one-dark.css'; 
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
